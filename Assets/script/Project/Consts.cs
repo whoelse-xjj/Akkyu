f2e6174e5e658c5b9fe072adb77bde80abeca35f
whoelse-xjj <xjj121212@163.com>
2020-09-17 21:51:53 +0800

    aaa

--- Assets/script/Project/Consts.cs
+++ Assets/script/Project/Consts.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine.UI;
 
 namespace Assets.Scenes.script.Project
 {
@@ -10,5 +11,15 @@ namespace Assets.Scenes.script.Project
     {
         public const float MoveTime = 2F;
         public const float MoveSpeed= 0.1F;
+        
+        public class Scene
+        {
+            public const int InitialScene = 0;
+            public const int BotanicalGarden = 4;
+            public const int Pond = 2;
+            public const int PrivateSchool = 3;
+            public const int MainScene = 1;
+        }
+
     }
 }
