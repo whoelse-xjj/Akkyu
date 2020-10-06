cc6a01ce7c0368f16085bc854e9489b9c2dc8594
whoelse-xjj <xjj121212@163.com>
2020-10-06 20:22:11 +0800

    数据存储

--- Assets/scripts/MainScene/DataManager.cs
+++ Assets/scripts/MainScene/DataManager.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
 
 public class DataManager : MonoBehaviour
 {
     public Inventory Inventory;
 
     private void Start() =>
-        DataSave();
-
-    private void OnDestroy() =>
         DataLoad();
 
-    private void DataSave()
+    private void OnDestroy() =>
+        DataSave();
+
+    private void DataLoad()
     {
         //C:/ Users / xjj12 / AppData / LocalLow / DefaultCompany / Fairy Ranch of Akyuu
         //           var path1 = Application.persistentDataPath;
@@ -24,9 +25,18 @@ public class DataManager : MonoBehaviour
         //    var path3 = new FileInfo("./").FullName;
         //    Debug.Log($"{path1}, {path2}, {path3}");
 
+        string path = Application.persistentDataPath + "/GameData/Inventory.txt";
+        BinaryFormatter formatter = new BinaryFormatter();
+        if (File.Exists(path))
+        {
+            FileStream stream = File.Open(path, FileMode.Open);
+            string json = formatter.Deserialize(stream).ToString();
+            JsonUtility.FromJsonOverwrite(json, Inventory);
+            stream.Close();
+        }
     }
 
-    private void DataLoad()
+    private void DataSave()
     {
         string path = Application.persistentDataPath + "/GameData";
 
@@ -38,7 +48,9 @@ public class DataManager : MonoBehaviour
         BinaryFormatter formatter = new BinaryFormatter();
         FileStream stream = File.Create(path + "/Inventory.txt");
         string json = JsonUtility.ToJson(Inventory);
+        //string json = formatter.Deserialize(stream).ToString();
 
         formatter.Serialize(stream, json);
+        stream.Close();
     }
 }
